8322678ab674961dde53c414fb0b7e80eca7b920
Hasan Can Büyükaşık <hasan.buyukasik@veripark.com>
2020-06-08 16:54:07 +0300

    useless files removed

--- app/src/main/java/com/example/dagger2example/models/Album.kt
+++
@@ -1,3 +0,0 @@
-package com.example.dagger2example.models
-
-data class Album(val id: Int, val userId: Int, val title: String)
--- app/src/main/java/com/example/dagger2example/models/User.kt
+++
@@ -1,3 +0,0 @@
-package com.example.dagger2example.models
-
-data class User(val id: Int, val name: String, val username: String)
